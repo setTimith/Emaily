73b51690ef724c4b6aca01cc8f6f3fd2a9521958
setTimith <tabboud997@gmail.com>
2022-06-05 11:23:13 +0300

    Fixes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -19,32 +22,45 @@ module.exports = (app) => {
     res.send(surveys);
   });
 
-  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
-    if (req.credits) {
-      const { title, subject, body, recipients } = req.body;
-
-      const survey = new Survey({
-        title,
-        subject,
-        body,
-        recipients: recipients.split(",").map((email) => ({
-          email: email.trim(),
-        })),
-        _user: req.user.id,
-        dateSent: Date.now(),
-      });
-      const mailer = new Mailer(survey, surveyTemplates(survey));
-
-      try {
-        await mailer.send();
-        await survey.save();
-        req.user.credits -= 1;
-        const user = await req.user.save();
-
-        res.send(user);
-      } catch (err) {
-        res.status(422).send(err);
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const events = _.map(req.body, (event) => {
+      const pathname = new URL(event.url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email: event.email,
+          surveyId: match.surveyId,
+          choice: match.choice,
+        };
       }
+    });
+  });
+
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map((email) => ({
+        email: email.trim(),
+      })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+    const mailer = new Mailer(survey, surveyTemplates(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
     }
   });
 };
